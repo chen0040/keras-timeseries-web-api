c00e2a53a9fc37f5399e89e26a17dc3db0572fe4
xschen <xs0040@gmail.com>
2017-09-24 10:03:26 +0800

    add celery

--- keras_timeseries_web/flaskr.py
+++ keras_timeseries_web/flaskr.py
@@ -1,9 +1,6 @@
 import os
-from keras_image_classifier_web.cnn_bi_classifier import BiClassifier
-from keras_image_classifier_web.cifar10_classifier import Cifar10Classifier
-from keras_image_classifier_web.vgg16_classifier import VGG16Classifier
-from keras_image_classifier_web.vgg19_classifier import VGG19Classifier
 
+from celery import Celery
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
 from flask import send_from_directory
@@ -12,28 +9,40 @@ from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+
+    class ContextTask(TaskBase):
+        abstract = True
+
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+
 app = Flask(__name__)  # create the application instance :)
 app.config.from_object(__name__)  # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+celery = make_celery(app)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-bi_classifier = BiClassifier()
-bi_classifier.run_test()
-
-cifar10_classifier = Cifar10Classifier()
-cifar10_classifier.run_test()
-
-vgg16_classifier = VGG16Classifier()
-vgg16_classifier.run_test()
-
-vgg19_classifier = VGG19Classifier()
-vgg19_classifier.run_test()
 
 @app.route('/')
 def classifiers():
-    return render_template('classifiers.html')
+    return render_template('home.html')
 
 
 def allowed_file(filename):
@@ -41,7 +50,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-def store_uploaded_image(action):
+def store_uploaded_file(action):
     # check if the post request has the file part
     if 'file' not in request.files:
         flash('No file part')
@@ -64,70 +73,20 @@ def about():
     return 'about us'
 
 
-@app.route('/cats_vs_dogs', methods=['GET', 'POST'])
+@app.route('/timeseries', methods=['GET', 'POST'])
 def cats_vs_dogs():
     if request.method == 'POST':
-        return store_uploaded_image('cats_vs_dogs_result')
-    return render_template('cats_vs_dogs.html')
+        return store_uploaded_file('timeseries_result')
+    return render_template('timeseries.html')
 
 
-@app.route('/cifar10', methods=['GET', 'POST'])
-def cifar10():
-    if request.method == 'POST':
-        return store_uploaded_image('cifar10_result')
-    return render_template('cifar10.html')
 
-
-@app.route('/vgg16', methods=['GET', 'POST'])
-def vgg16():
-    if request.method == 'POST':
-        return store_uploaded_image('vgg16_result')
-    return render_template('vgg16.html')
-
-
-@app.route('/vgg19', methods=['GET', 'POST'])
-def vgg19():
-    if request.method == 'POST':
-        return store_uploaded_image('vgg19_result')
-    return render_template('vgg19.html')
-
-
-@app.route('/cats_vs_dogs_result/<filename>')
-def cats_vs_dogs_result(filename):
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    probability_of_dog, predicted_label = bi_classifier.predict(filepath)
-    return render_template('cats_vs_dogs_result.html', filename=filename,
-                           probability_of_dog=probability_of_dog, predicted_label=predicted_label)
-
-
-@app.route('/cifar10_result/<filename>')
+@app.route('/timeseries_result/<filename>')
 def cifar10_result(filename):
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    predicted_class, predicted_label = cifar10_classifier.predict(filepath)
-    return render_template('cifar10_result.html', filename=filename,
-                           predicted_class=predicted_class, predicted_label=predicted_label)
+    return render_template('timeseries_result.html', filename=filename)
 
 
-@app.route('/vgg16_result/<filename>')
-def vgg16_result(filename):
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    top3 = vgg16_classifier.predict(filepath)
-    return render_template('vgg16_result.html', filename=filename,
-                           top3=top3)
-
-
-@app.route('/vgg19_result/<filename>')
-def vgg19_result(filename):
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    top3 = vgg19_classifier.predict(filepath)
-    return render_template('vgg19_result.html', filename=filename,
-                           top3=top3)
-
-
-@app.route('/images/<filename>')
-def get_image(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
 
 
 if __name__ == '__main__':
