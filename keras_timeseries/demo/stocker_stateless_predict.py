ae7b737c7667b1d41b1ccdcdb2ec728b97245f0b
xschen <xs0040@gmail.com>
2018-01-20 12:48:39 +0800

    stocker train and predict scripts demo

---
+++ keras_timeseries/demo/stocker_stateless_predict.py
@@ -0,0 +1,32 @@
+import quandl
+
+from keras_timeseries.library.plot_utils import plot_timeseries
+from keras_timeseries.library.recurrent import StatelessLSTM
+
+mydata = quandl.get("WIKI/MSFT")
+# mydata = quandl.get("WIKI/MSFT", returns="numpy")
+
+print(mydata.head())
+
+timeseries = mydata.as_matrix(['Close']).T[0]
+
+print(timeseries.shape)
+
+network = StatelessLSTM()
+
+model_dir_path = '../models/stocker'
+
+network.load_model(model_dir_path=model_dir_path)
+
+predicted_list = []
+actual_list = []
+timesteps = 6
+for i in range(timeseries.shape[0] - timesteps - 1):
+    X = timeseries[i:i + timesteps].T
+    predicted = network.predict(X)
+    actual = timeseries[i + timesteps + 1]
+    predicted_list.append(predicted)
+    actual_list.append(actual)
+    print('predicted: ' + str(predicted) + ' actual: ' + str(actual))
+
+plot_timeseries(actual_list, predicted_list, StatelessLSTM.model_name)
---
+++ keras_timeseries/demo/stocker_stateless_train.py
@@ -0,0 +1,18 @@
+import quandl
+
+from keras_timeseries.library.recurrent import StatelessLSTM
+
+mydata = quandl.get("WIKI/MSFT")
+# mydata = quandl.get("WIKI/MSFT", returns="numpy")
+
+print(mydata.head())
+
+timeseries = mydata.as_matrix(['Close']).T[0]
+
+print(timeseries.shape)
+
+network = StatelessLSTM()
+
+output_dir_path = '../models/stocker'
+timesteps = 6
+network.fit(timeseries, model_dir_path=output_dir_path, num_timesteps=timesteps, epochs=10)
